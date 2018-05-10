51dfde54f68a2c1e1043b6af7d9de211f7ddb97c
genboo <gen.masj@gmail.com>
2018-05-10 23:30:02 +0300

    Initial commit

---
+++ app/src/main/java/ru/devsp/app/locator/App.kt
@@ -0,0 +1,14 @@
+package ru.devsp.app.locator
+
+import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Fabric.with(this, Crashlytics())
+    }
+
+}
---
+++ app/src/main/java/ru/devsp/app/locator/MainActivity.kt
@@ -0,0 +1,31 @@
+package ru.devsp.app.locator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
